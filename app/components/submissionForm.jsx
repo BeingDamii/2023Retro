e6b89fa9443c0ba56f2326f63566880678f1f275
Damii dennison <dwanedehydra@gmail.com>
2023-11-06 02:37:15 +0100

    connected form elements to state and partially preconfigured the request shema

--- app/components/submissionForm.jsx
+++ app/components/submissionForm.jsx
@@ -2,21 +2,64 @@
 import { useState } from "react";
 
 const SubmissionForm = () => {
-  const [content, setContent] = useState();
+  const [submission, setSubmission] = useState("");
+  const [name, setName] = useState("");
+  const [twitter, setTwitter] = useState(false);
 
-  const handleChange = (e) => {
-    setContent(e.target.value);
+  // event handlers
+  const handleSubmissionChange = (e) => {
+    setSubmission(e.target.value);
   };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleTwitterChange = (e) => {
+    setTwitter(e.target.checked);
+  };
+
+  const handleClick = () => {
+    const post = {
+      submission,
+      name,
+      twitter,
+    };
+    console.log(post);
+  };
+
   return (
     <div className="submission-form-wrapper">
       <textarea
         className="submission-textarea"
-        value={content}
-        onChange={handleChange}
+        value={submission}
+        onChange={handleSubmissionChange}
         placeholder="Say something..."
       ></textarea>
       <div className="submission-form-container">
-        <button className="button button-primary">Say it</button>
+        <button className="button-primary" onClick={handleClick}>
+          Say it
+        </button>
+        <div className="other-form">
+          <input
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+            className="username-input"
+            placeholder="@ Your username"
+          />
+          <div className="checkbox-container">
+            <input
+              type="checkbox"
+              checked={twitter}
+              onChange={handleTwitterChange}
+              className="checkbox"
+              name="twitter"
+              id=""
+            />
+            <h3>This is my X username</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ dist/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oFaWU00X/uKPV/WAyNgxbgOBXzFDBySSwquQ7l9zhIc=\"\n}"
